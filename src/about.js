ce2c011cd0e7ff0cfdfd4bc17474a2c8fdadc393
João Oliveira <jaoovit@gmail.com>
2024-04-06 13:46:54 +0100

    create a infoBox to arange the about content

--- src/about.js
+++ src/about.js
@@ -9,18 +9,22 @@ export function displayAbout() {
     contact.textContent = 'Contact Us';
     content.appendChild(contact);
 
+    const infoBox = document.createElement('div');
+    infoBox.setAttribute('id', 'infoBox');
+    content.appendChild(infoBox)
+
     const chef = document.createElement('div');
     chef.setAttribute('id', 'chef');
     chef.innerHTML = '<h4>Chef</h4><p>555 555 555</p><p>exemplo@email.com</p>';
-    content.appendChild(chef);
+    infoBox.appendChild(chef);
 
     const manager = document.createElement('div');
     manager.setAttribute('id', 'manager');
     manager.innerHTML = '<h4>Manager</h4><p>555 555 556</p><p>exemplo@email.com</p>';
-    content.appendChild(manager);
+    infoBox.appendChild(manager);
 
     const location = document.createElement('div');
     location.setAttribute('id', 'location');
     location.innerHTML = '<h4>Location</h4><p>Avenida Exemplo, nº55, Not Real City</p>';
-    content.appendChild(location);
+    infoBox.appendChild(location);
 };
